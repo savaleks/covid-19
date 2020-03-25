8ce3e8853c92bd7c63620d78d4caff72403ac1b6
savaleks <savaleks@gmail.com>
2020-03-25 12:20:09 +0200

    Updates

---
+++ precache-manifest.59cf8cbaf567245ae23bd2e40012238f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b68cd54f3686a8e033721e1d7dce5a",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "00ac92b096eb86c6bd9d",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56f75155cd0c17861a70",
+    "url": "/reactapp/static/js/2.7df45a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.7df45a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ac92b096eb86c6bd9d",
+    "url": "/reactapp/static/js/main.612e4dc7.chunk.js"
+  },
+  {
+    "revision": "171d446e0b58cec8ce57",
+    "url": "/reactapp/static/js/runtime-main.4ea19c2d.js"
+  }
+]);
