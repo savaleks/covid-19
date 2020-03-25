09bc381272679a17100a3fe5b4a3dbba71eade2a
savaleks <savaleks@gmail.com>
2020-03-25 12:44:10 +0200

    changed app file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-      <p>This is example for testing purpose.</p>
-    </div>
-  );
+export default class App extends React.Component {
+
+    state = {
+        allCountries: []
+    }
+
+    componentDidMount() {
+        Axios.get(`https://covid19.mathdro.id/api/countries`)
+        .then(res => 
+            res.data.countries.map(country => ({
+                name: `${country.name}`
+            }))
+        )
+        .then(allCountries => {
+            this.setState({
+                allCountries
+            });
+        })
+    }
+
+    render() {
+        const {allCountries} = this.state;
+        return (
+            <div>{allCountries.map((country) => {
+                return (
+                    <p>{country.name}</p>
+                )
+            })}</div>
+        )
+    }
 }
-
-export default App;
