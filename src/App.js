9b6085c9d0ca64da12a1076c7b855adc3920eaf5
savaleks <savaleks@gmail.com>
2020-03-25 12:18:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <p>This is example for testing purpose.</p>
+    </div>
+  );
+}
+
+export default App;
