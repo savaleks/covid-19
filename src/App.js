f572e493cea415981e80411a50eaeb3ae9995b62
savaleks <savaleks@gmail.com>
2020-03-27 13:18:58 +0200

    added donation button

--- src/App.js
+++ src/App.js
@@ -85,6 +85,13 @@ export default class App extends React.Component {
             <h4>Paskutinis atnaujinimas</h4>
             <Moment fromNow>{this.state.lastUpdate}</Moment>
           </div>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="savaleks@gmail.com" />
+            <input type="hidden" name="currency_code" value="EUR" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_LT/i/scr/pixel.gif" width="1" height="1" />
+          </form>
         <div>
           <select className="dropdown" onChange={this.getCountryData}>
             <option>Pasaulyje</option>
